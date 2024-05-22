2ae8c1cc0061415806201ee4f85eff9b628be89c
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-22 18:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 //complete this code
 class Person {
-	constructor(name,age)
-	{
+	constructor(name,age){
 		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		return name
-	}
-	set age(age){
 		this.age=age;
 	}
 }
 
 class Student extends Person {
-	constructor(name,age);
-	super();
 	study(){
-		console.log(name+" is studying")
+		console.log(this.name+" is studying")
 	}
 	
 	
 }
 
 class Teacher extends Person {
-	constructor(name,age);
-	super();
 	teach(){
-		console.log(name+" is teaching")
+		console.log(this.name+" is teaching")
 	}
 }
 
